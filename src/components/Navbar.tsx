45f34e0d80d7abfdb6c2ae207792453e94aed0cb
Ashu Sikriwal <hashu94@gmail.com>
2024-11-30 19:22:02 +0530

    fix: img

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,7 +66,7 @@ const NavBar = () => {
       <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between p-4">
           <div className="flex items-center gap-7">
-            <Image width={0} height={0} src="/img/logo.png" alt="logo" className="w-10" />
+            <Image width={50} height={50} src="/img/logo.png" alt="logo" className="w-10" />
 
             <Button
               id="product-button"
